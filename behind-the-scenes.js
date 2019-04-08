ea442b82aebf25ecb5518388a6982fc213c52fa2
Sahil Tandon <sahil.tandon@live.com>
2019-04-08 02:34:26 -0400

    behing-the-scenes | explains the hidden module wrapping function and how node works behind the scenes.

---
+++ behind-the-scenes.js
@@ -0,0 +1,8 @@
+/*(function(exports, require, module, __filename, __dirname) {*/
+	/*Every file is considered as a module by node.*/
+	/*The commented out wrapping IIFE represents what every file in node actually looks like. It gives an idea of how node works behind the scenes.*/
+		
+		console.log("See this and the reveal-path module in your editor to view comments that explain how node works behind the scenes.");
+
+	/*exports is actually just a reference to module.exports. So you can always write just exports.xyz insted of module.exports.xyz. Though you cannot change exports reference, i.e. you cannot write exports = xyz instead of module.exports = xyz.*/
+/*});*/
